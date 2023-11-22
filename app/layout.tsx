b21143e31470bab92ca91db57f5ae72a9bd760e3
roberponzetti <roberponzetti@gmail.com>
2023-11-21 23:07:15 -0300

    initialized next14 demo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import '@/app/ui/global.css'; //el arroba hace referencia al root de nuestra aplicacion, evita poner ../../../
+import { inter } from '@/app/ui/fonts';
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
